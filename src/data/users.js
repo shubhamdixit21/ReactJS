d5c6bcc5662a8481a0496a23dcd7d8e2dd89a9cb
Shubham Dixit <shubhamrajneshdixit@gmail.com>
2022-05-25 19:29:30 +0530

    Created condition for empty values

--- src/data/users.js
+++ src/data/users.js
@@ -15,22 +15,22 @@ class User extends Component{
         let {users, searchKey} = this.state;
 
         let filterUsers = users.filter((user)=>{
-            var isFound = user.name.includes(searchKey);
+            var isFound = user.name.includes(searchKey) || user.username.includes(searchKey) || user.website.includes(searchKey);
             if(isFound){
-                return user.name;
-            }
-            console.log("Is Found", isFound);
+                return user;
+            } 
+            // console.log("Is Found", isFound);
         })
         this.setState({users:filterUsers})
-        console.log(filterUsers);
-        console.log("You are searching for ",searchKey);
+        // console.log(filterUsers);
+        // console.log("You are searching for ",searchKey);
     }
     
     inputHandle = (event) => {
         let searchString = event.target.value;
         this.setState({searchKey : searchString});
         // console.log(event.target.value);
-        console.log(searchString);
+        // console.log(searchString);
     }
     
     render(){
@@ -38,7 +38,15 @@ class User extends Component{
         /** Destructuring **/
         /** Extract variable with values **/
         let {users}=this.state;
-        console.log(users);
+        // console.log(users);
+        let isEmpty = Object.keys(users).length;
+        let result;
+        if (isEmpty > 0){
+            result = <Datafound userdata={users} />
+        }else{
+            result = <Datanotfound />
+        }
+        console.log(isEmpty);
         return(
             <div>
                 <h1>{Heading}</h1>
@@ -47,30 +55,46 @@ class User extends Component{
                     <input type="text" name="search" placeholder="Enter name to search" onChange={this.inputHandle.bind(this)}></input>
                     <button onClick={this.search.bind(this)}>Search</button>
                 </div>
-                <table>
-                    <thead>
-                        <tr>
-                            <td>ID</td>
-                            <td>Name</td>
-                            <td>User Name</td>
-                            <td>Phone</td>
-                            <td>Website</td>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {users.map((user, key)=>{
-                          return  <tr key={key}>
-                            <td>{user.id}</td>
-                            <td>{user.name}</td>
-                            <td>{user.username}</td>
-                            <td>{user.phone}</td>
-                            <td>{user.website}</td>
-                        </tr>
-                        })}
-                        
-                    </tbody>
-                </table>
+                {result}
             </div>
+        )
+    }
+}
+
+class Datafound extends Component{
+    render(){
+        console.log(this.props);
+        let users = this.props.userdata;
+        return(<table>
+            <thead>
+                <tr>
+                    <td>ID</td>
+                    <td>Name</td>
+                    <td>User Name</td>
+                    <td>Phone</td>
+                    <td>Website</td>
+                </tr>
+            </thead>
+            <tbody>
+                {users.map((user, key)=>{
+                  return  <tr key={key}>
+                    <td>{user.id}</td>
+                    <td>{user.name}</td>
+                    <td>{user.username}</td>
+                    <td>{user.phone}</td>
+                    <td>{user.website}</td>
+                </tr>
+                })}
+                
+            </tbody>
+        </table>)
+    }
+}
+
+class Datanotfound extends Component{
+    render(){
+        return(
+            <div>Data Not Found</div>
         )
     }
 }
